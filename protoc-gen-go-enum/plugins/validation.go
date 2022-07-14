056e0dbb903a8949b053256f771fdd0674d488a7
guader <nampp@live.com>
2022-07-14 15:01:03 +0800

    add README.md

--- protoc-gen-go-enum/plugins/validation.go
+++ protoc-gen-go-enum/plugins/validation.go
@@ -13,13 +13,13 @@ func (gen *Validation) GeneratedFilenameSuffix() string {
 func (gen *Validation) Generate(g *protogen.GeneratedFile, enums []*protogen.Enum) error {
 	for _, enum := range enums {
 		name := enum.Desc.Name()
-		g.P("// IsValid returns whether the ", name, " is a defined value")
+		g.P("// IsValid return whether the ", name, " is a defined value")
 		g.P("func (x ", name, ") IsValid() bool {")
 		g.P("_, ok := ", name, "_name[int32(x)]")
 		g.P("return ok")
 		g.P("}")
 		g.P()
-		g.P("// IsDefault returns whether the ", name, " is the default value")
+		g.P("// IsDefault return whether the ", name, " is the default value")
 		g.P("func (x ", name, ") IsDefault() bool {")
 		g.P("return x == 0")
 		g.P("}")
