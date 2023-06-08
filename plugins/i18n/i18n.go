d4d634b243db2ddf6a4012655e40e1a9b68c46d3
Guader <nampp@live.com>
2023-06-08 10:05:55 +0800

    i18n

--- plugins/i18n/i18n.go
+++ plugins/i18n/i18n.go
@@ -24,117 +24,117 @@ func Generate(plugin *protogen.Plugin) error {
 			continue
 		}
 
-		objectsByLang := make(map[string]map[string][][2]string)
-		translationsForEnums(objectsByLang, file.Enums)
-		translationsForMessages(objectsByLang, file.Messages)
-		translations := make([]translation, 0, len(objectsByLang))
-		for lang, fieldsByTypeName := range objectsByLang {
-			objects := make([]object, 0, len(fieldsByTypeName))
-			for typeName, fields := range fieldsByTypeName {
-				objects = append(objects, object{
-					name:   typeName,
-					fields: fields,
-				})
-			}
-			sort.SliceStable(objects, func(i, j int) bool {
-				return objects[i].name < objects[j].name
-			})
-			translations = append(translations, translation{
-				lang:    lang,
-				objects: objects,
-			})
-		}
-		sort.SliceStable(translations, func(i, j int) bool {
-			return translations[i].lang < translations[j].lang
-		})
-		if len(translations) == 0 {
-			continue
-		}
-
 		suffix := strings.TrimSpace(fileOpts.GetSuffix())
 		if len(suffix) == 0 {
 			suffix = ".ext.i18n"
 		}
 
-		g := plugin.NewGeneratedFile(fmt.Sprintf("%s%s.ts", file.GeneratedFilenamePrefix, suffix),
+		g := plugin.NewGeneratedFile(fmt.Sprintf("%s%s.go", file.GeneratedFilenamePrefix, suffix),
 			file.GoImportPath)
 		g.P(pkg.RenderPackageComments(version.Version, "i18n", file.Desc.Path()))
-		g.P("namespace ", file.GoPackageName, "I18n {")
-		for _, t := range translations {
-			g.P()
-			g.P("  export const ", t.lang, " = {")
-			for _, o := range t.objects {
-				g.P("    ", o.name, ": {")
-				for _, f := range o.fields {
-					g.P(fmt.Sprintf(`      %s: %q,`, f[0], f[1]))
-				}
-				g.P("    },")
-			}
-			g.P("  }")
-		}
-		g.P("}")
+		g.P("package ", file.GoPackageName)
+
+		generateEnums(g, file.Enums)
+		generateMessages(g, file.Messages)
 	}
 	return nil
 }
 
-func translationsForEnums(objectsByLang map[string]map[string][][2]string, es []*protogen.Enum) {
+type (
+	language     = protoreflect.Name
+	translations = map[string]string
+)
+
+func generateEnums(g *protogen.GeneratedFile, es []*protogen.Enum) {
 	for _, e := range es {
-		objectName := e.GoIdent.GoName
+		fields := make([]string, 0)
+		transesByLang := make(map[language]translations)
 		for _, v := range e.Values {
 			opts := pkg.ProtoGetExtension[i18n.I18N](v.Desc.Options(), i18n.E_Enum)
 			if opts == nil {
 				continue
 			}
-			fieldName := string(v.Desc.Name())
+			field := exportName(string(v.Desc.Name())) // Want shorter name but exported.
+			fields = append(fields, field)
 			opts.ProtoReflect().Range(func(d protoreflect.FieldDescriptor, v protoreflect.Value) bool {
-				lang := string(d.Name())
-				fieldsByObjectName, ok := objectsByLang[lang]
+				lang := d.Name()
+				transes, ok := transesByLang[lang]
 				if !ok {
-					fieldsByObjectName = make(map[string][][2]string)
-					objectsByLang[lang] = fieldsByObjectName
+					transes = make(translations)
+					transesByLang[lang] = transes
 				}
-				fieldsByObjectName[objectName] = append(fieldsByObjectName[objectName], [2]string{fieldName, v.String()})
+				transes[field] = v.String()
 				return true
 			})
 		}
+		if len(fields) == 0 {
+			continue
+		}
+		generateI18n(g, e.GoIdent.GoName, fields, transesByLang)
 	}
 }
 
-// map[string]map[string][][2]string
-// map[LANG]map[OBJECT][]{FIELD,TEXT}
-func translationsForMessages(objectsByLang map[string]map[string][][2]string, ms []*protogen.Message) {
+func generateMessages(g *protogen.GeneratedFile, ms []*protogen.Message) {
 	for _, m := range ms {
-		translationsForEnums(objectsByLang, m.Enums)
-
-		objectName := m.GoIdent.GoName
+		fields := make([]string, 0)
+		transesByLang := make(map[language]translations)
 		for _, f := range m.Fields {
 			opts := pkg.ProtoGetExtension[i18n.I18N](f.Desc.Options(), i18n.E_Field)
 			if opts == nil {
 				continue
 			}
-			fieldName := string(f.Desc.Name())
+			field := f.GoName
+			fields = append(fields, field)
 			opts.ProtoReflect().Range(func(d protoreflect.FieldDescriptor, v protoreflect.Value) bool {
-				lang := string(d.Name())
-				fieldsByObjectName, ok := objectsByLang[lang]
+				lang := d.Name()
+				transes, ok := transesByLang[lang]
 				if !ok {
-					fieldsByObjectName = make(map[string][][2]string)
-					objectsByLang[lang] = fieldsByObjectName
+					transes = make(translations)
+					transesByLang[lang] = transes
 				}
-				fieldsByObjectName[objectName] = append(fieldsByObjectName[objectName], [2]string{fieldName, v.String()})
+				transes[field] = v.String()
 				return true
 			})
 		}
-
-		translationsForMessages(objectsByLang, m.Messages)
+		if len(fields) == 0 {
+			continue
+		}
+		generateI18n(g, m.GoIdent.GoName, fields, transesByLang)
+		generateEnums(g, m.Enums)
+		generateMessages(g, m.Messages)
 	}
 }
 
-type object struct {
-	name   string
-	fields [][2]string
+func generateI18n(g *protogen.GeneratedFile, typ string, fields []string, transesByLang map[language]translations) {
+	Prefix := "I18n_" + typ // Export.
+	prefix := "i18n_" + typ // Unexport.
+	g.P()
+	g.P("type ", Prefix, " interface {")
+	for _, field := range fields {
+		g.P(field, "() string")
+	}
+	g.P("}")
+
+	langs := make([]language, 0, len(transesByLang))
+	for lang := range transesByLang {
+		langs = append(langs, lang)
+	}
+	sort.SliceStable(langs, func(i, j int) bool {
+		return langs[i] < langs[j]
+	})
+	for _, lang := range langs {
+		g.P()
+		g.P(fmt.Sprintf(`var %s_%s %s = (*%s_%s)(nil)`, Prefix, lang, Prefix, prefix, lang))
+		g.P()
+		g.P(fmt.Sprintf(`type %s_%s struct{}`, prefix, lang))
+		g.P()
+		transes := transesByLang[lang]
+		for _, field := range fields {
+			g.P(fmt.Sprintf(`func (*%s_%s) %s() string { return %q }`, prefix, lang, field, transes[field]))
+		}
+	}
 }
 
-type translation struct {
-	lang    string
-	objects []object
+func exportName(name string) string {
+	return strings.ToUpper(name[:1]) + name[1:]
 }
