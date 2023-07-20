bbebd1b6cf0c84d217bdafd332edd42178a8e513
Guader <nampp@live.com>
2023-07-20 17:29:06 +0800

    add validation func for enums

--- plugins/enums/enums.go
+++ plugins/enums/enums.go
@@ -42,12 +42,44 @@ func Generate(plugin *protogen.Plugin) error {
 func generateEnums(g *protogen.GeneratedFile, es []*protogen.Enum) {
 	for _, e := range es {
 		typ := e.GoIdent.GoName
+
+		// Generate enum slice.
 		g.P("var ", typ, "_enums = []", typ, "{")
 		for _, v := range e.Values {
 			g.P(v.GoIdent.GoName, ",")
 		}
 		g.P("}")
 		g.P()
+
+		// Generate validate function.
+		g.P("func (x ", typ, ") IsValid() bool {")
+		g.P("_, ok := ", typ, "_name[int32(x)]")
+		g.P("return ok")
+		g.P("}")
+		g.P()
+
+		//enumOpts := pkg.ProtoGetExtension[enums.EnumOptions](e.Desc.Options(), enums.E_EnumOptions)
+
+		// Generate parser functions.
+		//number := enumOpts.GetParser().GetDefault()
+		//value := e.Values[0]
+		//for _, v := range e.Values {
+		//	if int32(v.Desc.Number()) == number {
+		//		value = v
+		//		break
+		//	}
+		//}
+		//number = int32(value.Desc.Number())
+		//
+		//g.P("func (x *", typ, ") FromString(s string) {")
+		//g.P("if i, ok := ", typ, "_value[s]; ok {")
+		//g.P("*x = ", typ, "(i)")
+		//g.P("} else {")
+		//g.P("// Default value: ", number, ".")
+		//g.P("*x = ", value.GoIdent.GoName)
+		//g.P("}")
+		//g.P("}")
+		//g.P()
 	}
 }
 
