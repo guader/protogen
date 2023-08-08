0065ac39379a61e374232ec578cc9fa752c1b8dd
Guader <nampp@live.com>
2023-08-08 11:59:41 +0800

    remove unuseful plugins

--- cmd/protoc-gen-go-ext/main.go
+++ cmd/protoc-gen-go-ext/main.go
@@ -4,14 +4,10 @@ import (
 	"google.golang.org/protobuf/compiler/protogen"
 	"google.golang.org/protobuf/types/pluginpb"
 
-	"github.com/guader/protogen/plugins/accessor"
 	"github.com/guader/protogen/plugins/consts"
 	"github.com/guader/protogen/plugins/enums"
-	"github.com/guader/protogen/plugins/errorer"
-	"github.com/guader/protogen/plugins/getter"
 	"github.com/guader/protogen/plugins/i18n"
 	"github.com/guader/protogen/plugins/setter"
-	"github.com/guader/protogen/plugins/validator"
 )
 
 func main() {
@@ -22,13 +18,9 @@ func generate(plugin *protogen.Plugin) error {
 	plugin.SupportedFeatures = uint64(pluginpb.CodeGeneratorResponse_FEATURE_PROTO3_OPTIONAL)
 	for _, f := range []func(*protogen.Plugin) error{
 		setter.Generate,
-		validator.Generate,
-		errorer.Generate,
 		consts.Generate,
 		i18n.Generate,
 		enums.Generate,
-		getter.Generate,
-		accessor.Generate,
 	} {
 		if err := f(plugin); err != nil {
 			return err
